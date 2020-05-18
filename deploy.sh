6bf82ee4bc9bd20c2fbeb877ef2ac8fecae00dcb
neevparikh <neev_parikh@brown.edu>
2020-05-18 10:21:21 -0400

    Updated deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
+
+echo -e "\033[0;32mDeleting old content...\033[0m"
+rm -rf public
+
+echo -e "\033[0;32mChecking out branch...\033[0m"
+git worktree add public master
+
+echo -e "\033[0;32mGenerating site...\033[0m"
 hugo
-git add public/
-git add --all
-git commit -m "$(date) - updating site"
-git subtree pull --prefix public https://github.com/neevparikh/neevparikh.github.io master --squash
-git subtree push --prefix public https://github.com/neevparikh/neevparikh.github.io master
+
+echo -e "\033[0;32mDeploying branch...\033[0m"
+cd public &&
+    git add --all &&
+    git commit -m "$(date) - updating site" &&
+    git push origin master --force
+
+echo -e "\033[0;32mUpdating source...\033[0m"
 git push origin source
+
+echo -e "\033[0;32mCleaning up...\033[0m"
+git worktree remove public

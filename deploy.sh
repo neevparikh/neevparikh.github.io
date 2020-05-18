9115971d20fe0d3de8c50c5406e6e73721e44615
neevparikh <neev_parikh@brown.edu>
2020-05-17 21:06:41 -0400

    Updated deploy

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@
 hugo
 git add public/
 git commit -m "$(date) - updating site"
+git subtree pull --prefix public https://github.com/neevparikh/neevparikh.github.io master --squash
 git subtree push --prefix public https://github.com/neevparikh/neevparikh.github.io master

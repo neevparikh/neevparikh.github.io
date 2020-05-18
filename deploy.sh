3ea3f589bc6bdd163e14418c3491551543fd8803
neevparikh <neev_parikh@brown.edu>
2020-05-17 21:21:26 -0400

    Sun 17 May 2020 09:21:26 PM EDT - updating site

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,4 @@ git add --all
 git commit -m "$(date) - updating site"
 git subtree pull --prefix public https://github.com/neevparikh/neevparikh.github.io master --squash
 git subtree push --prefix public https://github.com/neevparikh/neevparikh.github.io master
+git push origin source

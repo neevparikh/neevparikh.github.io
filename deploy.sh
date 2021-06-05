b5b01ce414a277475447ed73a6952979209dc2a3
Neev Parikh <neev_parikh@brown.edu>
2021-06-05 12:56:55 -0400

    Sat Jun  5 12:56:55 PM EDT 2021 - updating source

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env bash
 
-echo -e "\033[0;32mDeleting old content...\033[0m"
-rm -rf public
+if command -v hugo >/dev/null 2>&1; then
+    echo -e "\033[0;32mDeleting old content...\033[0m"
+    rm -rf public
+    echo -e "\033[0;32mChecking out branch...\033[0m"
+    git worktree add public master
 
-echo -e "\033[0;32mChecking out branch...\033[0m"
-git worktree add public master
+    echo -e "\033[0;32mGenerating site...\033[0m"
+    hugo
 
-echo -e "\033[0;32mGenerating site...\033[0m"
-hugo
+    echo -e "\033[0;32mDeploying branch...\033[0m"
+    cd public &&
+        git add --all &&
+        git commit -m "$(date) - updating site" &&
+        git push origin master --force
+    cd ..
 
-echo -e "\033[0;32mDeploying branch...\033[0m"
-cd public &&
-    git add --all &&
-    git commit -m "$(date) - updating site" &&
-    git push origin master --force
-cd ..
+    echo -e "\033[0;32mCleaning up...\033[0m"
+    git worktree remove public
 
-echo -e "\033[0;32mCleaning up...\033[0m"
-git worktree remove public
-
-echo -e "\033[0;32mUpdating source...\033[0m"
-git add --all
-git commit -m "$(date) - updating source"
-git push origin source
+    echo -e "\033[0;32mUpdating source...\033[0m"
+    git add --all
+    git commit -m "$(date) - updating source"
+    git push origin source
+fi
